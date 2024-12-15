0c9145b1ad67f0c1aae05a01fe1b16f856dcc039
Shalu Jha <sjha18788@gmail.com>
2024-12-15 15:29:13 +0530

    Update secondHighestNumber.js

--- secondHighestNumber.js
+++ secondHighestNumber.js
@@ -1,25 +1,43 @@
-const greaterThan = function (array, number) {     
-  if (number > array[0]) {                
-    array[1] = array[0];                           
-    array[0] = number;                           
-  } 
-
-  if (number > array[1] && array[0] > number) {  
-    array[1] = number;      
+const getWinnerRunnerUp = function ([highest, secondHighest], number) {
+  if (number > highest) {
+    secondHighest = highest;
+    highest = number;
   }
 
-  return array; 
-}
+  if (number > secondHighest && highest > number) {
+    secondHighest = number;
+  }
 
-function getSecondHighest(collection, array) {
+  return [highest, secondHighest];
+};
+
+function secondHighest(collection, [highest, secondHighest]) {
   let result = [];
-  result = collection.reduce(greaterThan, array);
-  
+  result = collection.reduce(getWinnerRunnerUp, [highest, secondHighest],);
+
   return result[1];
 }
 
-console.log(getSecondHighest([4, 3, 2, 1], [0, 0])) ;
-console.log(getSecondHighest([1, 2, 3, 4], [0, 0]));
-console.log(getSecondHighest([1, 2, 4, 3], [0, 0]));
-console.log(getSecondHighest([1, 2, 5], [0, 0])); 
-console.log(getSecondHighest([8, 2, 5], [0, 0])); 
+function getMark(expected, actual) {
+  return actual === expected ? '✅' : '❌';
+}
+
+function testSecondHighest(collection, initArray, expected) {
+  const actual = secondHighest(collection, initArray);
+  const mark = getMark(expected, actual);
+
+  console.log(mark, 'actual :', actual, 'expected :', expected);
+}
+
+function testAll() {
+  testSecondHighest([4, 3, 2, 1], [-Infinity, -Infinity], 3);
+  testSecondHighest([1, 2, 3, 4], [-Infinity, -Infinity], 3);
+  testSecondHighest([1, 2, 6, 4], [-Infinity, -Infinity], 4);
+  testSecondHighest([1, 2, 5], [-Infinity, -Infinity], 2);
+  testSecondHighest([8, 2, 5], [-Infinity, -Infinity], 5);
+  testSecondHighest([-1, -2, -3], [-Infinity, -Infinity], -2);
+  testSecondHighest([0, -1, -3], [-Infinity, -Infinity], -1);
+  testSecondHighest([-1, -3], [-Infinity, -Infinity], -3);
+}
+
+testAll();
